342ed73e0b20f3c6a99213dfa95abd3e488b126c
Dn. Shvts <thedanny2005@gmail.com>
2024-12-08 15:06:59 +0200

    update

---
+++ 2 lesson/hw2/homework/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.MistyRose;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 125);
+            label1.TabIndex = 0;
+            label1.MouseDown += label1_MouseDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(586, 356);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ 2 lesson/hw2/homework/Form1.cs
@@ -0,0 +1,59 @@
+﻿namespace homework
+{
+    public partial class Form1 :Form {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Настройка размеров окна
+            this.Width = 500;
+            this.Height = 400;
+
+            // Настройка размеров и расположения метки
+            label1.Width = this.ClientSize.Width - 20;
+            label1.Height = this.ClientSize.Height - 20;
+            label1.Location = new Point(10, 10);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Отображение координат курсора
+            Text = $"X = {e.X}, Y = {e.Y}";
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Отображение текущих размеров формы
+                Text = $"{this.Width} x {this.Height}";
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("Вы нажали снаружи прямоугольника");
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Определение границ прямоугольника
+            bool isOnLeftEdge = e.Location.X == 0;
+            bool isOnTopEdge = e.Location.Y == 0;
+            bool isOnRightEdge = e.Location.X == label1.Width - 1;
+            bool isOnBottomEdge = e.Location.Y == label1.Height - 1;
+
+            // Проверка нажатия на границу
+            if (e.Button == MouseButtons.Left && (isOnLeftEdge || isOnTopEdge || isOnRightEdge || isOnBottomEdge))
+            {
+                MessageBox.Show("Вы нажали на границу прямоугольника");
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                // Нажатие внутри прямоугольника
+                MessageBox.Show($"Вы нажали внутри прямоугольника\n" +
+                                $"Mouse: X: {e.Location.X}, Y: {e.Location.Y}\n" +
+                                $"Square: W: {label1.Width}, H: {label1.Height}");
+            }
+        }
+    }
+}
